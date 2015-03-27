dce7fdf58cbb925de8e230b62ff7701f3ff48af9
Jacek Spiewak <jacek.spiewak@gmail.com>
2015-03-27 09:58:14 +0000

    Logging added

--- watchman.py
+++ watchman.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import time
 import redis
 import yaml
+import logging
 from subprocess import call
 
 from daemon import runner
@@ -16,7 +17,7 @@ class Watchman():
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
-        self.pidfile_path = '/tmp/sentinel_listener.pid'
+        self.pidfile_path = '/tmp/watchman.pid'
         self.pidfile_timeout = 5
         self.nutcracker_config = {}
         self.masterchange = {'prev': [], 'curr': []}
@@ -26,7 +27,11 @@ class Watchman():
 
     def run(self):
 
+        logger.info('Watchman started')	
+
         self.__read_configuration()
+
+        logger.info('Watchman configuration: {}'.format(str(self.config)))
 
         self.get_nutcracker_config()
 
@@ -37,6 +42,8 @@ class Watchman():
         while True:
             message = p.get_message()
             if message and message['channel'] == '+switch-master' and message['type'] == 'pmessage':
+
+                logger.info('Got +switch-master message')
                 
                 self.get_masterchange(message)
                 self.update_nutcracker_config()
@@ -53,6 +60,8 @@ class Watchman():
         self.masterchange['prev'] = msgdata[1:3]
         self.masterchange['curr'] = msgdata[3:]
 
+        logger.info('Master change: {}'.format(str(self.masterchange)))
+
     def update_nutcracker_config(self):
 
         servers = self.nutcracker_config[self.config['clustername']]['servers']
@@ -66,16 +75,28 @@ class Watchman():
                 self.nutcracker_config[self.config['clustername']]['servers'][index] = '{}:{}:{} {}'.format(
                     self.masterchange['curr'][0], self.masterchange['curr'][1], num, servername)
 
+                logger.info('Nutcracker configuration updated')
+
     def save_nutcracker_config(self):
         yaml.dump(self.nutcracker_config, file(self.config['nutcracker_config_file'], 'w'), default_flow_style=False)
+        logger.info('Nutcracker configuration saved')
 
     
     def restart_nutcracker(self):
         call(['/etc/init.d/nutcracker', 'restart'])
+        logger.info('Nutcracker restarted')
 
-if __name__ == '__main__':
 
-    watchman = Watchman()
-    daemon_runner = runner.DaemonRunner(watchman)
-    daemon_runner.daemon_context.working_directory = '/etc/watchman'
-    daemon_runner.do_action()
+watchman = Watchman()
+
+logger = logging.getLogger("watchmanlog")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = logging.FileHandler("/var/log/watchman/watchman.log")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+daemon_runner = runner.DaemonRunner(watchman)
+daemon_runner.daemon_context.working_directory = '/etc/watchman'
+daemon_runner.daemon_context.files_preserve=[handler.stream]
+daemon_runner.do_action()
